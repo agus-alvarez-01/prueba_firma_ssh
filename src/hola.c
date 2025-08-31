1dd310cf1d84250613e25b2de3bb6a47bbf56c16
agus-alvarez-01 <agus.alvarez2001@mi.unc.edu.ar>
2025-08-31 17:58:32 -0300

    22

--- src/hola.c
+++ src/hola.c
@@ -2,15 +2,5 @@
 
 int main(){
     printf("Hola Sistemas Operativos I\n");
-
-    int *puntero;
-    int numero = 10;
-    puntero = &numero;
-    printf("Numero: %d\n", numero);
-    printf("*puntero: %d\n", *puntero);
-    printf("y la direccion de memoria es: %d\n", puntero);
-    char i = 'I';
-
-    printf("Hola Sistemas Operativos " + i );
     return 0;
 }
